a5d303b0de4f1f9884e5fb9a9e35d875f1b995ff
Rafal <rafalgurbisz@outlook.com>
2020-09-29 07:41:01 +0200

    Fixed unused imports

--- src/Chat/components/LeftPanel.tsx
+++ src/Chat/components/LeftPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext } from "react"
 import styled from "styled-components"
 import firebase from '../../firebase'
 import { AuthContext } from "../../Auth/auth"
